dd17402d495376f2f3afad8c1f275e4113edc09e
kang <abcnt@naver.com>
2019-01-30 14:10:54 +0900

    대분류 mysql 에서 조회해서 view 화면 (index.ejs)에 뿌려주기
    대분류 mysql 에서 조회해서 view 화면 (index.ejs)에 뿌려주기
    *썸머노트로 작성한 문장 <h1>하하</h1> 이 db에 저장되고
    뿌려질때 &:lk 하하 %#;이런식으로 특수문자로 나오는 문제 ejs 쪽에서 해결

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var loger = require('../logmodule.js');           //로그모듈
-
+var client = require('../config/mysqlconfig.js'); //mysql 모듈
 loger.info("메모리 로딩 시작. - index.js");
 
 
 /* 메인화면 */
 router.get('/', function (req, res, next) {
-
-  res.render('index');
-
+  var sql = 'select * from bigTbl';
+  client.query(sql, function (err, rows, results) {
+    if(err){
+      loger.error('대분류 조회 문장에 오류가 있습니다. - / - /index.js');
+      loger.error(err);
+    }else{
+        if(rows.length > 0){
+            res.render('index',{
+              rows:rows
+            });
+        }else{
+          //대분류 제목이 없을 경우
+          var ud = undefined;
+          res.render('index',{
+            rows:ud
+          });
+        }
+    }
+  });
 });
 
 

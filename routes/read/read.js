5a71fbb42c96041166856ab08c31b02e92c7c33d
kang <abcnt@naver.com>
2019-01-30 17:10:52 +0900

    대분류 글 클릭했을때 url로 pk 값 넘겨서 상세 페이지 보여주기
    대분류 글 클릭했을때 url로 pk 값 넘겨서 상세 페이지 보여주기

--- routes/read/read.js
+++ routes/read/read.js
@@ -7,25 +7,43 @@ loger.info("메모리 로딩 시작. - read.js");
 
 /* 중분류 보기 */
 router.get('/read/readbigmiddle', function (req, res, next) {
+
+  var bignum = req.query.num;
+  loger.info(bignum);
+
   var sql = 'select * from bigTbl';
   client.query(sql, function (err, rows, results) {
     if(err){
       loger.error('대분류 조회 문장에 오류가 있습니다. - /read/readbigmiddle - /read.js');
       loger.error(err);
     }else{
-        if(rows.length > 0){
+
+      if(rows.length > 0){
+        var sql2 = 'select * from bigTbl where bignum = ?';
+        client.query(sql2, bignum,function (err, onerow, results) {
+          if(err){
+            loger.error('대분류 글 하나 조회 문장에 오류가 있습니다. - /read/readbigmiddle - /read.js');
+            loger.error(err);
+          }else{
             res.render('read/readbigmiddle',{
-              rows:rows
+              rows:rows,
+              onerow:onerow
             });
-        }else{
-          //대분류 제목이 없을 경우
-          var ud = undefined;
-          res.render('read/readbigmiddle',{
-            rows:ud
-          });
-        }
+          }
+        })
+
+    }else{
+      //대분류 제목이 없을 경우
+      var ud = undefined;
+      res.render('read/readbigmiddle',{
+        rows:ud,
+        onerow:ud
+      });
+    }
     }
   });
+
+
 });
 
 
